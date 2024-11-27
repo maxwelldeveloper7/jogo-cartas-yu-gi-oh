aab680e6c8843d477169aee5bd8bf4a847fad780
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-11-27 11:37:40 -0300

    implementando o setCardFeilds

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -76,6 +76,21 @@ async function createCardImage(IdCard, fieldSide) {
     return cardImage;
 }
 
+async function setCardsField(cardId) {
+    await removeAllCardsImages();
+    let computerCardId = await getRandomCardId();
+    state.fieldCards.player.style.display = "block";
+    state.fieldCards.computer.style.display = "block";
+
+    state.fieldCards.player.src = cardData[cardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+
+    let duelResults = await checkDuelResults(cardId, computerCardId);
+    
+    await updateScore();
+    await drawButton(duelResults);
+}
+
 async function drawSelectedCard(index) {
     state.cardSprites.avatar.src = cardData[index].img;
     state.cardSprites.name.innerText = cardData[index].name;
