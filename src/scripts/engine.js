b0cf131541f39e8e3b8e45151dac83cef9f0f800
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-11-27 08:41:29 -0300

    criando assinatura das funções principais

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,6 +18,11 @@ const state = {
     },
 };
 
+const playerSides = {
+    player1: "player-field-card",
+    computer: "computer-field-card"
+};
+
 const pathImages = "./src/assets/icons/"
 
 const cardData = [
@@ -47,9 +52,19 @@ const cardData = [
     }
 ]
 
+async function drawCards(cardNumber, fieldSide){
+    for(let i = 0; i < cardNumber; i++){
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+        
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
 
 function init(){
-
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
 }
 
 init();
